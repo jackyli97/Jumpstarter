abc978f77ea3b2434d1c1294eb0e43ccd083d29d
jackyli97 <xwlh808@gmail.com>
2020-10-14 15:01:32 -0700

    Fix issues

--- frontend/jumpstarter.jsx
+++ frontend/jumpstarter.jsx
@@ -6,20 +6,19 @@ import Root from "./components/root";
 import { login, logout, signup } from './actions/session_actions'
 
 document.addEventListener("DOMContentLoaded", () => {
-    // let store;
-    // if (window.currentUser) {
-    //     const preloadedState = {
-    //         entities: {
-    //             users: { [window.currentUser.id]: window.currentUser }
-    //         },
-    //         session: { id: window.currentUser.id }
-    //     };
-    //     store = configureStore(preloadedState);
-    //     delete window.currentUser;
-    // } else {
-    //     store = configureStore();
-    // }
-    const store = configureStore();
+    let store;
+    if (window.currentUser) {
+        const preloadedState = {
+            entities: {
+                users: { [window.currentUser.id]: window.currentUser }
+            },
+            session: { id: window.currentUser.id }
+        };
+        store = configureStore(preloadedState);
+        delete window.currentUser;
+    } else {
+        store = configureStore();
+    }
     const root = document.getElementById("root");
     ReactDOM.render(<Root store={store} />, root);
     window.getState = store.getState;
